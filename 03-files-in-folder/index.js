d8f663fba1fbbccc9e058ceece1865ed0b89478c
Maltsau <emeraldwhistler.dm@gmail.com>
2023-05-04 20:24:23 +0300

    feat: 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,29 +1,25 @@
-/* process.stdin - поток ввода; process.stdout - поток вывода; process.stderr - поток ошибки как разновидность потока вывода */
-
-// stdout.write("What is yuur name?\n"); /* console.log("What is yuur name?) */
-// /* subscribe on console event "data" of strdin object, that writes "data" to console */
-// stdin.on("data", (data) => {
-//   stdout.write(`Hello, ${data}`);
-//   exit();
-// });
-// process.on("exit", () => stdout.write("Удачи в изучении Node.js!\n"));
-
-// console.log(path.parse(__filename));
-// console.log(path.join(__dirname, "./index", "/README.md"));
-// console.log(__dirname);
-// console.log(process.argv);
-
-const { stdout, stdin, exit } = process;
+const { stdout } = process;
 const fs = require("fs");
 const os = require("os");
 const path = require("path");
 
+const eol = os.EOL;
 const { readdir, stat } = fs.promises;
 const folderPath = path.resolve(__dirname, "secret-folder");
 const filesObj = readdir(folderPath, { withFileTypes: true });
 filesObj.then((files) => {
   files.forEach((file) => {
-    console.log(file);
+    if (file.isFile()) {
+      const filePath = path.resolve(__dirname, "secret-folder", file.name);
+      const fileName = path.basename(filePath);
+      const fileStats = stat(filePath);
+      fileStats.then((data) => {
+        const fileNameArr = fileName.split(".");
+        const fileSize = data.size / 1024;
+        stdout.write(
+          `${fileNameArr[0]} - ${fileNameArr[1]} - ${fileSize}kb` + eol
+        );
+      });
+    }
   });
 });
-// console.log(filesObj);
