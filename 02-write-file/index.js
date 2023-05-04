76b101e1b72b04b99048c242d164841aa565737e
Maltsau <emeraldwhistler.dm@gmail.com>
2023-05-04 18:08:14 +0300

    fix: comment delete

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -23,17 +23,4 @@ process.on("SIGINT", () => {
   exit();
 });
 
-/* process.stdin - поток ввода; process.stdout - поток вывода; process.stderr - поток ошибки как разновидность потока вывода */
 
-// stdout.write("What is yuur name?\n"); /* console.log("What is yuur name?) */
-// /* subscribe on console event "data" of strdin object, that writes "data" to console */
-// stdin.on("data", (data) => {
-//   stdout.write(`Hello, ${data}`);
-//   exit();
-// });
-// process.on("exit", () => stdout.write("Удачи в изучении Node.js!\n"));
-
-// console.log(path.parse(__filename));
-// console.log(path.join(__dirname, "./index", "/README.md"));
-// console.log(__dirname);
-// console.log(process.argv);
