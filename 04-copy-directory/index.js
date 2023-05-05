36511573eb0384400381969bcc111027a320d448
Maltsau <emeraldwhistler.dm@gmail.com>
2023-05-05 09:47:12 +0300

    feat: 04-copy-directory done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,17 @@
+const { stdout } = process;
+const fs = require("fs");
+const os = require("os");
+const path = require("path");
+const { readdir, copyFile } = fs.promises;
 
+const srcPath = path.resolve(__dirname, "files");
+const distPath = path.resolve(__dirname, "files-copy");
+fs.mkdir(distPath, { recursive: true }, (err) => {
+  if (err) throw err;
+});
+const filesObj = readdir(srcPath);
+filesObj.then((files) => {
+  files.forEach((file) => {
+    copyFile(path.resolve(srcPath, file), path.resolve(distPath, file));
+  });
+});
