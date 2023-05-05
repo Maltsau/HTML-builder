ed72bbaa7e1c4b703550823a472e82e3363394e0
Maltsau <emeraldwhistler.dm@gmail.com>
2023-05-05 12:46:45 +0300

    feat: 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,6 +1,23 @@
-const { stdout } = process;
 const fs = require("fs");
 const os = require("os");
 const path = require("path");
-const { readdir, copyFile } = fs.promises;
+const { readdir } = fs.promises;
 
+const eol = os.EOL;
+const srcPath = path.resolve(__dirname, "styles");
+const distPath = path.resolve(__dirname, "project-dist", "bundle.css");
+
+const srcFilesObj = readdir(srcPath);
+const writeStream = fs.createWriteStream(distPath);
+
+srcFilesObj.then((files) => {
+  files.forEach((file) => {
+    const filePath = path.resolve(__dirname, "styles", file);
+    if (path.extname(filePath) === ".css") {
+      const readStream = fs.createReadStream(filePath, "utf-8");
+      readStream.on("data", (data) => {
+        writeStream.write(data.toString() + eol);
+      });
+    }
+  });
+});
