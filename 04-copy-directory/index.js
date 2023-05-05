9527323bc9b426ab852267b56600a41cd7822090
Maltsau <emeraldwhistler.dm@gmail.com>
2023-05-05 10:57:32 +0300

    fix: delete file handler on 04 add

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,16 +1,20 @@
-const { stdout } = process;
 const fs = require("fs");
-const os = require("os");
 const path = require("path");
-const { readdir, copyFile } = fs.promises;
+const { readdir, copyFile, unlink } = fs.promises;
 
 const srcPath = path.resolve(__dirname, "files");
 const distPath = path.resolve(__dirname, "files-copy");
 fs.mkdir(distPath, { recursive: true }, (err) => {
   if (err) throw err;
 });
-const filesObj = readdir(srcPath);
-filesObj.then((files) => {
+const distFilesObj = readdir(distPath);
+distFilesObj.then((files) => {
+  files.forEach((file) => {
+    if (file) unlink(path.resolve(distPath, file));
+  });
+});
+const srcFilesObj = readdir(srcPath);
+srcFilesObj.then((files) => {
   files.forEach((file) => {
     copyFile(path.resolve(srcPath, file), path.resolve(distPath, file));
   });
---
+++ 05-merge-styles/index.js
@@ -0,0 +1,6 @@
+const { stdout } = process;
+const fs = require("fs");
+const os = require("os");
+const path = require("path");
+const { readdir, copyFile } = fs.promises;
+
